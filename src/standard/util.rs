04c6498342be9682362e21ad6dc4c3a253e71d09
James Haver <mchaver@gmail.com>
2018-05-25 22:36:11 +0800

    Fix n

--- src/standard/util.rs
+++ src/standard/util.rs
@@ -1,29 +1,14 @@
 use standard::tables::*;
 use util;
 
-extern crate unicode_segmentation;
-
-use self::unicode_segmentation::UnicodeSegmentation;
-
 pub fn is_romaji(s: &str) -> bool {
     s.len() == 1 && ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","-"].contains(&s)
 }
 
-fn match_char(data: &char) -> bool {
-    match *data {
-        '\x01'...'\x08' |
-        '\u{10FFFE}'...'\u{10FFFF}' => true,
-        _ => false,
-    }
-}
-
-fn c_to_string(cs: Vec<(usize,char)>) -> String {
-    cs.into_iter().map(|(_, c)| c).collect()
-}
-
 pub fn romaji_to_hiragana_safe(romaji: &str) -> String {
-    let romaji_chars = UnicodeSegmentation::graphemes(romaji, true).collect::<Vec<&str>>();
-    let romaji_len = romaji_chars.len();
+    let romaji_chars = romaji.chars();
+    let romaji_len = romaji_chars.count();
+    let romaji_indices = romaji.char_indices().collect::<Vec<_>>();
 
     let mut romaji_index = 0;
     let mut window = 1; // 1 to 3
@@ -32,30 +17,35 @@ pub fn romaji_to_hiragana_safe(romaji: &str) -> String {
     // convert 1 to 3 ascii chars to a single hirgana
     // i is starting point, window is length to be looked up
     while romaji_index < romaji_len && romaji_index + window < romaji_len + 1 {
-        let romaji_sub_vec = &romaji_chars[romaji_index .. romaji_index + window];
-        let romaji_sub_string: String = romaji_sub_vec.join("");
+        let romaji_sub_vec = &romaji.chars().collect::<Vec<_>>()[romaji_index .. romaji_index + window];
+        let romaji_sub_string: String = romaji_sub_vec.into_iter().collect();
         let romaji_sub: &str = &romaji_sub_string[..];
 
-        let romaji_sub_vec_last = &romaji_chars[romaji_index + window - 1 .. romaji_index + window];
-        let romaji_sub_string_last: String = romaji_sub_vec_last.join("");
+        let romaji_sub_vec_last = &romaji.chars().collect::<Vec<_>>()[romaji_index + window - 1 .. romaji_index + window];
+        let romaji_sub_string_last: String = romaji_sub_vec_last.into_iter().collect();
         let romaji_sub_last: &str = &romaji_sub_string_last[..];
 
         if !is_romaji(romaji_sub_last) {
+            println!("{}",romaji_sub_last);
             hiragana = format!("{}{}", hiragana, romaji_sub);
             romaji_index += romaji_sub_vec.len();
 
             window = 1;
 
         } else {
-            // println!("{}", romaji_sub);
             // check for gemminate consonant, if it exists, consume the char
             // add a "っ" and skip the lookup
-            if window == 1 && romaji_index + 1 < romaji_len && util::is_consonant(romaji_sub) {
-                let romaji_peek_ahead_vec = &romaji_chars[romaji_index + 1 .. romaji_index + 2];
-                let romaji_peek_ahead: String = romaji_peek_ahead_vec.join("");
+            if window == 1 && romaji_index + 1 < romaji_len && (util::is_consonant(romaji_sub) || romaji_sub == "n") {
+                let romaji_peek_ahead_vec = &romaji.chars().collect::<Vec<_>>()[romaji_index + 1 .. romaji_index + 2];
+                let romaji_peek_ahead: String = romaji_peek_ahead_vec.into_iter().collect();
 
                 if romaji_sub == romaji_peek_ahead {
                     hiragana = format!("{}{}", hiragana, "っ");
+                    romaji_index += 1;
+                    window = 1;
+                    continue;
+                } else if romaji_sub == "n" {
+                    hiragana = format!("{}{}", hiragana, "ん");
                     romaji_index += 1;
                     window = 1;
                     continue;
@@ -80,8 +70,8 @@ pub fn romaji_to_hiragana_safe(romaji: &str) -> String {
                         }
                         window += 1;
                     } else {
-                        let romaji_sub_vec_first = &romaji_chars[romaji_index .. romaji_index + 1];
-                        let romaji_sub_string_first: String = romaji_sub_vec_first.join("");
+                        let romaji_sub_vec_first = &romaji.chars().collect::<Vec<_>>()[romaji_index .. romaji_index + 1];
+                        let romaji_sub_string_first: String = romaji_sub_vec_first.into_iter().collect();
                         let romaji_sub_first: &str = &romaji_sub_string_first[..];
                         
                         hiragana = format!("{}{}", hiragana, romaji_sub_first);
@@ -95,7 +85,6 @@ pub fn romaji_to_hiragana_safe(romaji: &str) -> String {
     
     hiragana.to_string()
 }
-
 
 // this expects everything to be romaji and is destructive
 pub fn romaji_to_hiragana(romaji: &str) -> String {
@@ -159,5 +148,6 @@ mod tests {
         assert_eq!(romaji_to_hiragana_safe("hri"), "hり".to_string());
         assert_eq!(romaji_to_hiragana_safe("sme"), "sめ".to_string());
         assert_eq!(romaji_to_hiragana_safe("wあり"), "wあり".to_string());
+        assert_eq!(romaji_to_hiragana_safe("nt"), "んt".to_string());
     }
 }
